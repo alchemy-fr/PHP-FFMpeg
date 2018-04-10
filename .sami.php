9b90c40a48d6b01f96b721e4bc33d67c41dbea73
Jean-Yves Gaulier <gaulier@alchemy.fr>
2018-04-10 19:31:04 +0200

    silex2 remove doc to commit before regenerate

--- .sami.php
+++ .sami.php
@@ -5,16 +5,25 @@ include 'vendor/autoload.php';
 use Sami\Sami;
 use Symfony\Component\Finder\Finder;
 
+$dir = 'src';
+
 $iterator = Finder::create()
     ->files()
     ->name('*.php')
-    ->in($dir = 'src')
+    ->in($dir)
+;
+
+$versions = GitVersionCollection::create($dir)
+    //->addFromTags('v2.0.*')
+    ->add('SILEX2', 'SILEX2 branch')
+    ->add('master', 'master branch')
 ;
 
 return new Sami($iterator, array(
     'title'                => 'PHP-FFMpeg API',
-    'theme'                => 'enhanced',
-    'build_dir'            => __DIR__.'/docs/source/API/API',
-    'cache_dir'            => __DIR__.'/docs/source/API/API/cache',
+    'versions'             => $versions,
+    //'theme'                => 'enhanced',
+    'build_dir'            => __DIR__.'/docs/source/API/API/%version%',
+    'cache_dir'            => __DIR__.'/docs/source/API/API/%version%/cache',
     'default_opened_level' => 2,
 ));
